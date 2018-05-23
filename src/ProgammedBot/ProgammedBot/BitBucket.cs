c158450d19f3bd2e2238f24758b4b41019151afd
Ben McCallum <ben.mccallum@live.com.au>
2018-05-23 21:44:55 +1000

    Stub out a basic message to Slack webhook.

--- src/ProgammedBot/ProgammedBot/BitBucket.cs
+++ src/ProgammedBot/ProgammedBot/BitBucket.cs
@@ -1,4 +1,3 @@
-
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -6,25 +5,39 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
 
 namespace ProgammedBot
 {
     public static class BitBucket
     {
+        public static HttpClient httpClient = new HttpClient();
+
         [FunctionName("BitBucket")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
-        {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
+        public async static Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {           
+            var requestBody = new StreamReader(req.Body).ReadToEnd();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var dataJson = JsonConvert.SerializeObject(data);
+            log.Info("C# HTTP trigger function processed a request. " + dataJson);
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            var response = await httpClient.PostAsJsonAsync<dynamic>(
+                Environment.GetEnvironmentVariable("SlackWebhookUrl"),
+                new
+                {
+                    payload = JsonConvert.SerializeObject(
+                        new
+                        {
+                            text = dataJson
+                        }
+                    )
+                }
+            );
+
+            return new OkResult();
         }
     }
 }
