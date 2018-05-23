733c2b2e7f1ce5969da2b6ebe21e9dc41353bf3e
Ben McCallum <ben.mccallum@live.com.au>
2018-05-23 23:21:13 +1000

    Implementing Slack messaging from BitBucket push outgoing webhook.

--- src/ProgammedBot/ProgammedBot/BitBucket.cs
+++ src/ProgammedBot/ProgammedBot/BitBucket.cs
@@ -5,39 +5,92 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
-using System.Net.Http;
 using System;
 using System.Threading.Tasks;
+using Slack.Webhooks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ProgammedBot
 {
     public static class BitBucket
     {
-        public static HttpClient httpClient = new HttpClient();
-
         [FunctionName("BitBucket")]
         public async static Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {           
             var requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            var data = JsonConvert.DeserializeObject<PushNotification>(requestBody);
+
             var dataJson = JsonConvert.SerializeObject(data);
-            log.Info("C# HTTP trigger function processed a request. " + dataJson);
+            log.Info("Received a request.");
 
+            var author = data.actor.display_name;
+            var changes = data.push.changes;
+            var numOfCommits = changes.Sum(c => Math.Max(c.truncated ? 5 : c.commits.Count, c.commits.Count));
+            var couldBeMoreCommits = changes.Any(c => c.truncated);
+            var repository = data.repository.name;
 
-            var response = await httpClient.PostAsJsonAsync<dynamic>(
-                Environment.GetEnvironmentVariable("SlackWebhookUrl"),
-                new
-                {
-                    payload = JsonConvert.SerializeObject(
-                        new
-                        {
-                            text = dataJson
-                        }
-                    )
-                }
-            );
+            var slackWebhookUrl = Environment.GetEnvironmentVariable("SlackWebhookUrl", EnvironmentVariableTarget.Process);
+            log.Info($"SlackWebhookUrl: {slackWebhookUrl}");
 
-            return new OkResult();
+            var slackClient = new SlackClient(slackWebhookUrl);
+
+            var slackMessage = new SlackMessage
+            {
+                Text = $":medal: {author} :medal: just pushed {(couldBeMoreCommits ? "at least " : "")}{numOfCommits} commit{(numOfCommits > 1 ? "s" : "")} to the {repository} repository. :clap::clap::clap:"
+            };
+
+            var success = await slackClient.PostAsync(slackMessage);
+            if (success)
+            {
+                var successMsg = "Success received from slack webhook. Message sent was: " + slackMessage.Text;
+                log.Info(successMsg);
+                return new OkObjectResult(successMsg);
+            }
+            else
+            {
+                log.Info("Error received from slack webhook. Message sent was: " + slackMessage.Text);
+                return new StatusCodeResult(500);
+            }
         }
+
+        public class PushNotification
+        {
+            public Push push { get; set; }
+            public Repository repository { get; set; }
+            public Actor actor { get; set; }
+        }
+
+        public class Push
+        {
+            public List<Change> changes { get; set; }
+        }
+
+        public class Change
+        {
+            public bool truncated { get; set; }
+            public List<Commit> commits { get; set; }
+        }
+
+        public class Commit
+        {
+            public string hash { get; set; }
+            public DateTime date { get; set; }
+            public string message { get; set; }
+            public string type { get; set; }
+        }
+
+        public class Repository
+        {
+            public string website { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Actor
+        {
+            public string username { get; set; }
+            public string display_name { get; set; }
+        }        
     }
 }
